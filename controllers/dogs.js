ee2cd096bbc89f2c7c765388b9924b2c0ea5a677
dimisavva <119276394+dimisavva@users.noreply.github.com>
2023-02-17 08:07:10 -0800

    complete CRUD for Dog model

--- controllers/dogs.js
+++ controllers/dogs.js
@@ -10,7 +10,51 @@ try {
   }
 }
 
+const index = async (req, res) => {
+  try{
+    const dogs = await Dog.findAll()
+    res.status(200).json(dogs)
+  }catch (error) {
+    res.status(500).json(error)
+  }
+}
+
+const show = async (req, res) => {
+  try{
+    const dog = await Dog.findByPk(req.params.id)
+    res.status(200).json(dog)
+  }catch (error) {
+    res.status(500).json(error)
+  }
+}
+
+const update = async (req, res) => {
+  try {
+    const dog = await Dog.findByPk(req.params.id)
+      dog.set(req.body)
+      await dog.save()
+      console.log(dog)
+    res.status(200).json(dog)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
+
+const deleteDog = async (req, res) => {
+  try {
+    // We can also call destroy on an instance:
+    const dog = await Dog.findByPk(req.params.id)
+    await dog.destroy()
+    res.status(200).json(dog)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
 
 module.exports = {
-create
+create,
+index,
+show,
+update,
+delete: deleteDog
 }
--- routes/dogs.js
+++ routes/dogs.js
@@ -1,6 +1,11 @@
 const router = require('express').Router()
 const dogsCtrl = require('../controllers/dogs.js')
 
+router.get('/', dogsCtrl.index)
+router.get('/:id', dogsCtrl.show)
 router.post('/', dogsCtrl.create)
+router.put('/:id', dogsCtrl.update)
+router.delete('/:id', dogsCtrl.delete)
+
 
 module.exports = router
